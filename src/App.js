8b7726a2118a2d969a505b82573efcc0dbfc16f9
thuhang1999 <thuhang1999@gmail.com>
2021-09-24 21:20:06 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { commerce } from './lib/commerce';
+
+//import Products from './components/Products/Products';
+//import Navbar from './components/Navbar/Navbar';
+import {Products, Navbar} from './components';
+const App = () => {
+    const [products, setProducts] = useState([]);
+    const fetchProducts = async() => {
+        const {data} = await commerce.products.list();
+        setProducts(data);
+
+    }
+    useEffect(() => {
+        fetchProducts();
+    }, []
+    );
+    console.log(products);
+    return (
+    <div>
+        <Navbar />
+        <Products products ={products}/>
+    </div>
+    )
+
+}
+export default App;
