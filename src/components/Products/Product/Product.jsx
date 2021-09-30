243d8320f8ae6e6a01b8005adde6a1cc57823016
kyunkakata <kyunkakata@gmail.com>
2021-09-30 13:29:10 +0700

    git push

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,35 +1,51 @@
-import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
-import { AddShoppingCart} from '@material-ui/icons';
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
 
-import useStyles from './styles';
+import useStyles from "./styles";
 
-const Product = ({product, onAddToCart}) => {
-    const classes = useStyles();
-    // console.log(product);
-    // return <div>test</div>
-        return (
-        <Card className={classes.root}> 
-            <CardMedia className={classes.media} image='{product.media.sourch}' title={product.name}/>
-            <CardContent>
-                <div className={classes.cardContent}>
-                    <Typography variant='hS' gutterBottom>
-                        {product.name}
-                    </Typography>
-                    <Typography variant='hS'>
-                        {product.price.formatted_with_symbol}
-                    </Typography>
-                </div>
-                <Typography dangerouslySetInnerHTML={{__html: product.desciption}} variant='body2' color='textSecondary'/>
-            </CardContent>
-            <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label='Add to Card' onClick={()=> onAddToCart(product.id,1)}>
-                    <AddShoppingCart/>
-                </IconButton>
-            </CardActions>
-
-        </Card>
-    );
-
-}
+const Product = ({ product, onAddToCart }) => {
+  const classes = useStyles();
+  console.log(product);
+  // return <div>test</div>
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={product.media.source}
+        title={product.name}
+      />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="hS" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="hS">
+            {product.price.formatted_with_symbol}
+          </Typography>
+        </div>
+        <Typography
+          dangerouslySetInnerHTML={{ __html: product.desciption }}
+          variant="body2"
+          color="textSecondary"
+        />
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton
+          aria-label="Add to Card"
+          onClick={() => onAddToCart(product.id, 1)}
+        >
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
 export default Product;
