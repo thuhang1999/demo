1dc11b5d64fbbdc716b603e2641d6a3d3db30c75
thuhang1999 <thuhang1999@gmail.com>
2021-10-06 22:49:34 +0700

    commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,22 @@
 const express = require("express");
+// const expressValidator = require('express-validator');
+const expressFlash = require('express-flash');
+const expressSession = require('express-session');
+
 const mysql = require("mysql");
 const app = express();
+
+// configure
+app.use(expressSession({
+  secret: 'secret_key',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+app.use(expressFlash());
+// app.use(expressValidator());
+
+
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -61,12 +77,35 @@ app.get("/api/order?a=b&c=d", (req, res) => {
 // user control
 //sign up
 app.get("/api/user/sign_up", (req, res) => {
-  res.json({ query: req.query });
+  let query = `INSERT INTO nguoi_dung(ten_user, ten_dang_nhap, mat_khau) VALUES ('KyunLegend','kyun','bootstraps3_v8');`;
+  connection.query(query, function (err, results){
+    if(err){
+      res.json({err_code: 400, err_msg: ""});
+    } 
+    res.json({"success": true});
+  })
 });
 
 // login
 app.get("/api/user/login", (req, res) => {
-  res.json({ query: req.query });
+  let query = `SELECT * FROM nguoi_dung WHERE nguoi_dung.ten_dang_nhap = "${req.query?.username}" AND nguoi_dung.mat_khau = "${req.query?.password}";`;
+  connection.query(query, function (err, rows, fields) {
+    if (err) throw err;
+    if (rows.length <= 0) {
+      res.json({
+        data: {
+          login_result: false,
+        }
+      })
+    } else {
+      res.json({
+        data: {
+          login_result: true,
+          user_info: rows[0]
+        }
+      })
+    }
+  })
 });
 
 //get user detail
